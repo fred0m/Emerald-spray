537cda78ae531b5cf304d304e115abbf6f34232a
fred0m <41663893+fred0m@users.noreply.github.com>
2019-10-09 21:16:04 +0800

    Add files via upload

---
+++ Emerald-spray/BugReporter.py
@@ -0,0 +1,22 @@
+#encoding=utf-8
+#---------------------------------------------------------------------------------------------------
+#Copyright: Copyright (c) 2019
+#Created on 2019-10-08
+#Author:fredom
+#Version 1.0
+#---------------------------------------------------------------------------------------------------
+import requests
+import os
+import sys
+class BugReporter :
+    def __init__(self) :
+        if (os.path.exists('error.log')):
+            errorlog = open ('error.log' , mode = 'r' , encoding = "utf-8")
+            sender = errorlog.read()
+            requests.get("https://sc.ftqq.com/SCU44058T44cff9901b6466371f4ae9be28645f945c57ed47b8bb0.send?text=软件错误报告 & desp="+sender)
+            errorlog.close()
+            os.remove("error.log")
+            quit()
+        else :
+            requests.get("https://sc.ftqq.com/SCU44058T44cff9901b6466371f4ae9be28645f945c57ed47b8bb0.send?text=软件错误报告 & desp=未成功获取到错误报告")
+            quit()
