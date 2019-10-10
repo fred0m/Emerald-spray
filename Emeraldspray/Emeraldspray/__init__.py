72c4300ad5b373b5c43b76ac4da16f5b41949a2c
fred0m <39172522@qq.com>
2019-10-10 21:47:49 +0800

    支持使用pip包管理工具安装

--- Emeraldspray/Emeraldspray/__init__.py
+++
@@ -1,7 +0,0 @@
-import re
-import os
-import requests
-from bs4 import BeautifulSoup
-from BugReporter import *
-from DataAnalysis import *
-from WebGeter import *
---
+++ Emeraldspray/__init__.py
@@ -0,0 +1,7 @@
+import re
+import os
+import requests
+from bs4 import BeautifulSoup
+from BugReporter import *
+from DataAnalysis import *
+from WebGeter import *
