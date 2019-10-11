7d14d51456c15256fec7e15cbafd9b79e8ae4894
fred0m <39172522@qq.com>
2019-10-11 11:52:13 +0800

    v1.0.2更新

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup
 
 setup(
-    name='Emerald-spray',
-    version='1.0.0',
+    name='Emeraldspray',
+    version='1.0.2',
     author='fredom',
     author_email='he@fredom.ink',
     url='https://github.com/fred0m/Emerald-spray',
     description=u'方便的将steam商店页面信息解析出来，并保存到本地',
-    packages=['EmeraldSpray'],
+    packages=['Es'],
     install_requires=["requests","BeautifulSoup4"]
 )
