c578c59bdbe80280ed77ef8f08b51e695621934b
iTsuabsa <lixuesonlu@qq.com>
2019-11-02 17:18:47 +0800

    2.1

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -17,6 +17,7 @@ class DataAnalysis :
                 }
 
     def steam_dataHandle(self) -> (dict) :
+        data = {}
         # 游戏名
         data["Gamename"] = self.raw_data.find("div", class_ = "apphub_AppName").string
         # 开发商
@@ -29,3 +30,27 @@ class DataAnalysis :
         data['Description'] = re.search(r"(.*)\t(.*\S)", str(self.raw_data.find("div", class_ = "game_description_snippet").string)).groups()[1]
         # 价格
         data['Price'] = re.search(r"(.*)\t(.*\S)", str(self.raw_data.find("div", class_ = "game_purchase_price price").string)).groups()[1]
+        # 配置需求
+        # 防止没有配置要求
+        try :
+            try :
+                data_ = self.raw_data.find("div", class_ = "game_area_sys_req_full")
+                data["Configuration"] = self.load(data_)
+            except :
+                right_data = self.raw_data.find("div", class_ = "game_area_sys_req_rightCol")
+                left_data = self.raw_data.find("div", class_ = "game_area_sys_req_leftCol")
+                data["Configuration"] = [self.load(left_data), self.load(right_data)]
+        except :
+            data["Configuration"] = {}
+        # 标签
+        data["tag"] = []
+        for i in self.raw_data.find_all("a", class_ = "app_tag") :
+            data["tag"].append(re.search(r"(.*)\t(.*\S)", str(i.string)).groups()[1])
+        return data
+    def __load(self, data) :
+        about = {}
+        for i in data.find_all("li") :
+            about[str(re.search(r"strong>(.*?)<", str(i)).groups()[0])] = re.search(r"strong> (.*?)<", str(i)).groups()[0]
+        type_ = re.search(r"strong>(.*?)<", str(data)).groups()[0]
+        dict = {type_ : about}
+        return dict
